8297aad3b9d0ec81dc8715b23adaf798c7d424c1
Rolando Fernandez <rolando@ebi.ac.uk>
2020-05-21 11:23:11 +0100

    simplified graph crawling in lieu of new links.json

---
+++ exporter/graph/experiment_graph.py
@@ -0,0 +1,166 @@
+from exporter.metadata import MetadataResource
+
+from copy import deepcopy
+from typing import List, Set, Dict, Iterable
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ProtocolLink:
+    protocol_type: str
+    protocol_uuid: str
+
+    @staticmethod
+    def from_metadata_resource(metadata: MetadataResource) -> 'ProtocolLink':
+        return ProtocolLink(metadata.concrete_type(), metadata.uuid)
+
+    def to_dict(self) -> Dict:
+        return dict(
+            protocol_type=self.protocol_type,
+            protocol_id=self.protocol_uuid
+        )
+
+
+@dataclass
+class Input:
+    input_type: str
+    input_uuid: str
+
+    def to_dict(self) -> Dict[str, str]:
+        return dict(
+            input_type=self.input_type,
+            input_id=self.input_uuid
+        )
+
+
+@dataclass
+class Output:
+    output_type: str
+    output_uuid: str
+
+    def to_dict(self) -> Dict[str, str]:
+        return dict(
+            output_type=self.output_type,
+            output_id=self.output_uuid
+        )
+
+
+class Link:
+
+    def __init__(self, process_uuid: str, process_type: str,
+                 inputs: Iterable[Input], outputs: Iterable[Output], protocols: Iterable[ProtocolLink]):
+        self._input_uuids: Set[str] = set()
+        self._outputs_uuids: Set[str] = set()
+        self._protocol_uuids: Set[str] = set()
+
+        self.process_uuid = process_uuid
+        self.process_type = process_type
+        self.inputs: Set[Input] = set()
+        self.outputs: Set[Output] = set()
+        self.protocols: Set[ProtocolLink] = set()
+
+        for i in inputs:
+            self.add_input(i)
+
+        for o in outputs:
+            self.add_output(o)
+
+        for p in protocols:
+            self.add_protocol(p)
+
+    def add_input(self, i: Input):
+        if i.input_uuid not in self._input_uuids:
+            self._input_uuids.add(i.input_uuid)
+            self.inputs.add(i)
+
+    def add_output(self, o: Output):
+        if o.output_uuid not in self._outputs_uuids:
+            self._outputs_uuids.add(o.output_uuid)
+            self.outputs.add(o)
+
+    def add_protocol(self, p: ProtocolLink):
+        if p.protocol_uuid not in self._protocol_uuids:
+            self._protocol_uuids.add(p.protocol_uuid)
+            self.protocols.add(p)
+
+    def combine_partial_link(self, link: 'Link'):
+        if self.process_type == link.process_type:
+            for i in link.inputs:
+                self.add_input(i)
+            for o in link.outputs:
+                self.add_output(o)
+            for p in link.protocols:
+                self.add_protocol(p)
+        else:
+            raise Exception(f'Cannot combine link (process_type: {self.process_type} with link of process_type {link.process_type}')
+
+    def to_dict(self) -> Dict:
+        return dict(
+            process_id=self.process_uuid,
+            process_type=self.process_type,
+            inputs=[i.to_dict() for i in self.inputs],
+            outputs=[o.to_dict() for o in self.outputs],
+            protocols=[p.to_dict() for p in self.protocols]
+        )
+
+
+class LinkSet:
+
+    def __init__(self):
+        self.links: Dict[str, Link] = dict()
+
+    def add_links(self, links: List[Link]):
+        for link in links:
+            self.add_link(link)
+
+    def add_link(self, link: Link):
+        link_uuid = link.process_uuid
+        if link_uuid in self.links:
+            self.links[link_uuid].combine_partial_link(link)
+        else:
+            self.links[link_uuid] = link
+
+    def get_links(self) -> List[Link]:
+        return list(self.links.values())
+
+
+class MetadataNodeSet:
+
+    def __init__(self):
+        self.obj_uuids = set()  # the uuid of a link is just the uuid of the process denoting the link
+        self.objs = []
+
+    def __contains__(self, item: MetadataResource):
+        return item.uuid in self.obj_uuids
+
+    def add_node(self, node: MetadataResource):
+        if node.uuid in self.obj_uuids:
+            pass
+        else:
+            self.obj_uuids.add(node.uuid)
+            self.objs.append(node)
+
+    def add_nodes(self, nodes: List[MetadataResource]):
+        for node in nodes:
+            self.add_node(node)
+
+    def get_nodes(self) -> List[MetadataResource]:
+        return [deepcopy(obj) for obj in self.objs]
+
+
+class ExperimentGraph:
+    links: LinkSet
+    nodes: MetadataNodeSet
+
+    def __init__(self):
+        self.links = LinkSet()
+        self.nodes = MetadataNodeSet()
+
+    def extend(self, graph: 'ExperimentGraph'):
+        for link in graph.links.get_links():
+            self.links.add_link(link)
+
+        for node in graph.nodes.get_nodes():
+            self.nodes.add_node(node)
+
+        return self
---
+++ exporter/graph/graph_crawler.py
@@ -0,0 +1,76 @@
+from ingest.api.ingestapi import IngestApi
+from exporter.metadata import MetadataResource
+from exporter.graph.experiment_graph import ExperimentGraph, Link, Input, Output, ProtocolLink
+from typing import List, Iterable, Dict
+from functools import reduce
+from _operator import iconcat
+from dataclasses import dataclass
+
+
+@dataclass
+class ProcessInfo:
+    process: MetadataResource
+    inputs: List[MetadataResource]
+    outputs: List[MetadataResource]
+    protocols: List[MetadataResource]
+
+
+class GraphCrawler:
+    def __init__(self, ingest_client: IngestApi):
+        self.ingest_client = ingest_client
+
+    def experiment_graph_for_process(self, process: MetadataResource) -> ExperimentGraph:
+        empty_graph = ExperimentGraph()
+        return self.__crawl(process, empty_graph)
+
+    def __crawl(self, process: MetadataResource, partial_graph: ExperimentGraph) -> ExperimentGraph:
+        process_info = self.process_info(process)
+        partial_graph.nodes.add_nodes(process_info.inputs + process_info.outputs + process_info.protocols + [process])
+        partial_graph.links.add_link(GraphCrawler.link_for(process_info))
+
+        processes_to_crawl = GraphCrawler.flatten([self.get_derived_by_processes(i) for i in process_info.inputs])
+        return reduce(lambda pg1, pg2: pg1.extend(pg2),
+                      map(lambda proc: self.__crawl(proc, partial_graph), processes_to_crawl),
+                      ExperimentGraph())
+
+    @staticmethod
+    def link_for(process_info: ProcessInfo):
+        link_inputs = [Input(i.concrete_type(), i.uuid) for i in process_info.inputs]
+        link_outputs = [Output(o.concrete_type(), o.uuid) for o in process_info.outputs]
+        link_protocols = [ProtocolLink(p.concrete_type(), p.uuid) for p in process_info.protocols]
+
+        process_type = process_info.process.concrete_type()
+        process_uuid = process_info.process.uuid
+        return Link(process_uuid, process_type, link_inputs, link_outputs, link_protocols)
+
+    @staticmethod
+    def flatten(list_of_lists: Iterable[Iterable]) -> List:
+        return reduce(iconcat, list_of_lists, [])
+
+    def get_derived_by_processes(self, experiment_material: MetadataResource) -> List[MetadataResource]:
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('derivedByProcesses', experiment_material.full_resource, 'processes'))
+
+    def get_derived_biomaterials(self, process: MetadataResource) -> List[MetadataResource]:
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('derivedBiomaterials', process.full_resource, 'biomaterials'))
+
+    def get_derived_files(self, process: MetadataResource) -> List[MetadataResource]:
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('derivedFiles', process.full_resource, 'files'))
+
+    def get_input_biomaterials(self, process: MetadataResource):
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('inputBiomaterials', process.full_resource, 'biomaterials'))
+
+    def get_input_files(self, process: MetadataResource):
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('inputFiles', process.full_resource, 'files'))
+
+    def get_protocols(self, process: MetadataResource) -> List[MetadataResource]:
+        return GraphCrawler.parse_metadata_resources(self.ingest_client.get_related_entities('protocols', process.full_resource, 'protocols'))
+
+    def process_info(self, process: MetadataResource) -> ProcessInfo:
+        inputs = self.get_input_biomaterials(process) + self.get_input_files(process)
+        outputs = self.get_derived_biomaterials(process) + self.get_derived_files(process)
+        protocols = self.get_protocols(process)
+        return ProcessInfo(process, inputs, outputs, protocols)
+
+    @staticmethod
+    def parse_metadata_resources(metadata_resources: List[Dict]) -> List[MetadataResource]:
+        return [MetadataResource.from_dict(m, True) for m in metadata_resources]
