38e11e1bb6c41085f0ca24fcad66a418cef18fcb
Rolando Fernandez <rolando@ebi.ac.uk>
2020-06-19 17:01:16 +0100

    added changes in lieu of the file_descriptors spec

--- exporter/schema.py
+++ exporter/schema.py
@@ -35,3 +35,13 @@ class SchemaService:
         )[0]
 
         return SchemaResource.from_dict(latest_schema)
+
+    def latest_file_descriptor_schema(self) -> SchemaResource:
+        latest_schema = self.ingest_client.get_schemas(
+            latest_only=True,
+            high_level_entity="system",
+            domain_entity="",
+            concrete_entity="file_descriptor"
+        )[0]
+
+        return SchemaResource.from_dict(latest_schema)
