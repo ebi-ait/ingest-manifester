3fcbab6388896e420f7d1fe759ef8b014b72b62d
Aclan Alegria <aaclan@ebi.ac.uk>
2020-10-08 11:06:57 +0100

    Move formatting of checksums to to_dict

--- exporter/terra/dcp_staging_client.py
+++ exporter/terra/dcp_staging_client.py
@@ -24,15 +24,19 @@ class FileDescriptor:
     checksums: FileChecksums
 
     def to_dict(self) -> Dict:
+        sha1 = self.checksums.sha1.lower() if self.checksums.sha1 else self.checksums.sha1
+        sha256 = self.checksums.sha256.lower() if self.checksums.sha256 else self.checksums.sha256
+        crc32c = self.checksums.crc32c.lower() if self.checksums.crc32c else self.checksums.crc32c
+
         return dict(
             file_id=self.file_uuid,
             file_version=self.file_version,
             file_name=self.file_name,
             content_type=self.content_type,
             size=self.size,
-            sha1=self.checksums.sha1,
-            sha256=self.checksums.sha256,
-            crc32c=self.checksums.crc32c,
+            sha1=sha1,
+            sha256=sha256,
+            crc32c=crc32c,
             s3_etag=self.checksums.s3_etag,
             schema_type='file_descriptor'
         )
@@ -40,9 +44,8 @@ class FileDescriptor:
     @staticmethod
     def from_file_metadata(file_metadata: MetadataResource) -> 'FileDescriptor':
         data_file = DataFile.from_file_metadata(file_metadata)
-        name = f'{data_file.uuid}_{data_file.dcp_version}_{data_file.file_name}'
-        file_version = utils.to_dcp_version(file_metadata.dcp_version)
-        return FileDescriptor(data_file.uuid, file_version, name,
+        name = f'{data_file.uuid}_{file_metadata.dcp_version}_{data_file.file_name}'
+        return FileDescriptor(data_file.uuid, file_metadata.dcp_version, name,
                               data_file.content_type, data_file.size, data_file.checksums)
 
 
