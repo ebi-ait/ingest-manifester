1ad984f269401efaa77ae2cc4b5135fe18b6e4fc
Jacob Windsor <me@jcbwndsr.com>
2020-11-08 13:16:26 +0100

    Pass project uuid into write funcs

--- exporter/terra/terra_exporter.py
+++ exporter/terra/terra_exporter.py
@@ -26,9 +26,9 @@ class TerraExporter:
         experiment_graph = self.graph_crawler.generate_experiment_graph(process, project)
         experiment_data_files = [DataFile.from_file_metadata(m) for m in experiment_graph.nodes.get_nodes() if m.metadata_type == "file"]
 
-        self.dcp_staging_client.write_metadatas(experiment_graph.nodes.get_nodes())
+        self.dcp_staging_client.write_metadatas(experiment_graph.nodes.get_nodes(), project.uuid)
         self.dcp_staging_client.write_links(experiment_graph.links, experiment_uuid, experiment_version, project.uuid)
-        self.dcp_staging_client.write_data_files(experiment_data_files)
+        self.dcp_staging_client.write_data_files(experiment_data_files, project.uuid)
 
     def export_update(self, metadata_urls: Iterable[str]):
         metadata_to_update = [self.metadata_service.fetch_resource(url) for url in metadata_urls]
