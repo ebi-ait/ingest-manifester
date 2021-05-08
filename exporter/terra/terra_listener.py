9d93d1468a4a214bcb21204ef30423cf870463aa
Alegria Aclan <aaclan@ebi.ac.uk>
2021-05-08 09:36:46 +0100

    Clean up, renaming and remove unused handling of updates

--- exporter/terra/terra_listener.py
+++ exporter/terra/terra_listener.py
@@ -17,17 +17,12 @@ import json
 class ExperimentMessageParseExpection(Exception):
     pass
 
-class SimpleUpdateMessageParseExpection(Exception):
-    pass
-
 
 @dataclass
 class ExperimentMessage:
     process_id: str
     process_uuid: str
     submission_uuid: str
-    experiment_uuid: str
-    experiment_version: str
     experiment_index: int
     total: int
     job_id: str
@@ -38,31 +33,11 @@ class ExperimentMessage:
             return ExperimentMessage(data["documentId"],
                                      data["documentUuid"],
                                      data["envelopeUuid"],
-                                     data["bundleUuid"],
-                                     data["versionTimestamp"],
                                      data["index"],
                                      data["total"],
                                      data["exportJobId"])
         except (KeyError, TypeError) as e:
             raise ExperimentMessageParseExpection(e)
-
-
-@dataclass
-class SimpleUpdateMessage:
-    metadata_urls: List[str]
-    submission_uuid: str
-    index: int
-    total: int
-
-    @staticmethod
-    def from_dict(data: Dict) -> 'SimpleUpdateMessage':
-        try:
-            return SimpleUpdateMessage(data["callbackLinks"],
-                                       data["envelopeUuid"],
-                                       data["index"],
-                                       data["total"])
-        except (KeyError, TypeError) as e:
-            raise SimpleUpdateMessageParseExpection(e)
 
 
 class _TerraListener(ConsumerProducerMixin):
@@ -72,14 +47,12 @@ class _TerraListener(ConsumerProducerMixin):
                  terra_exporter: TerraExporter,
                  job_service: TerraExportJobService,
                  experiment_queue_config: QueueConfig,
-                 update_queue_config: QueueConfig,
                  publish_queue_config: QueueConfig,
                  executor: ThreadPoolExecutor):
         self.connection = connection
         self.terra_exporter = terra_exporter
         self.job_service = job_service
         self.experiment_queue_config = experiment_queue_config
-        self.update_queue_config = update_queue_config
         self.publish_queue_config = publish_queue_config
         self.executor = executor
 
@@ -91,11 +64,7 @@ class _TerraListener(ConsumerProducerMixin):
                                         callbacks=[self.experiment_message_handler],
                                         prefetch_count=1)
 
-        update_consumer = _consumer([_TerraListener.queue_from_config(self.update_queue_config)],
-                                    callbacks=[self.update_message_handler],
-                                    prefetch_count=1)
-
-        return [experiment_consumer, update_consumer]
+        return [experiment_consumer]
 
     def experiment_message_handler(self, body: str, msg: Message):
         return self.executor.submit(lambda: self._experiment_message_handler(body, msg))
@@ -104,7 +73,7 @@ class _TerraListener(ConsumerProducerMixin):
         try:
             exp = ExperimentMessage.from_dict(json.loads(body))
             self.logger.info(f'Received experiment message for process {exp.process_uuid} (index {exp.experiment_index} for submission {exp.submission_uuid})')
-            self.terra_exporter.export(exp.process_uuid, exp.submission_uuid, exp.experiment_uuid, exp.experiment_version, exp.job_id)
+            self.terra_exporter.export(exp.process_uuid, exp.submission_uuid, exp.job_id)
             self.logger.info(f'Exported experiment for process uuid {exp.process_uuid} (--index {exp.experiment_index} --total {exp.total} --submission {exp.submission_uuid})')
             self.log_complete_assay(exp.job_id, exp.process_id)
 
@@ -118,19 +87,6 @@ class _TerraListener(ConsumerProducerMixin):
         except Exception as e:
             self.logger.error(f'Failed to export experiment message with body: {body}')
             self.logger.exception(e)
-
-    def update_message_handler(self, body: str, msg: Message):
-        return self.executor.submit(lambda: self._update_message_handler(body, msg))
-
-    def _update_message_handler(self, body: str, msg: Message):
-        update_message = SimpleUpdateMessage.from_dict(json.loads(body))
-        self.logger.info(f'Received metadata update message for submission {update_message.submission_uuid}) '
-                         f'(--index {update_message.index} --total {update_message.total})')
-        self.terra_exporter.export_update(update_message.metadata_urls)
-        self.logger.info(f'Exported updates for metadata URLs: [ {",".join(update_message.metadata_urls)} ] '
-                         f'(--index {update_message.index} --total {update_message.total})')
-
-        msg.ack()
 
     def log_complete_assay(self, job_id: str, assay_process_id: str):
         self.job_service.complete_assay(job_id, assay_process_id)
@@ -148,16 +104,14 @@ class TerraListener:
                  terra_exporter: TerraExporter,
                  job_service: TerraExportJobService,
                  experiment_queue_config: QueueConfig,
-                 update_queue_config: QueueConfig,
                  publish_queue_config: QueueConfig):
         self.amqp_conn_config = amqp_conn_config
         self.terra_exporter = terra_exporter
         self.job_service = job_service
         self.experiment_queue_config = experiment_queue_config
-        self.update_queue_config = update_queue_config
         self.publish_queue_config = publish_queue_config
 
     def run(self):
         with Connection(self.amqp_conn_config.broker_url()) as conn:
-            _terra_listener = _TerraListener(conn, self.terra_exporter, self.job_service, self.experiment_queue_config, self.update_queue_config, self.publish_queue_config, ThreadPoolExecutor())
+            _terra_listener = _TerraListener(conn, self.terra_exporter, self.job_service, self.experiment_queue_config, self.publish_queue_config, ThreadPoolExecutor())
             _terra_listener.run()
